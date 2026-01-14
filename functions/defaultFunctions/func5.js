9f5855e2a1a01bdf493d5055f29e609c95ad651d
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2026-01-14 19:50:23 +0300

    Обновление функции

--- functions/defaultFunctions/func5.js
+++ functions/defaultFunctions/func5.js
@@ -6,7 +6,7 @@ module.exports = async (ctx) => {
     ctx.session.waitingForFile = true;
     ctx.session.currentFunctions = 'func5';
     
-    await ctx.reply('Вы выбрали: 5');
+    await ctx.reply('*Вы выбрали: 5*', {parse_mode: "Markdown"});
     await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
         Markup.keyboard([
             ['Вернуться в меню']
