4b4765849c790fa3dade7ffa4a174af1948d5ed0
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2026-01-14 19:48:52 +0300

    Update func1.js

--- functions/defaultFunctions/func1.js
+++ functions/defaultFunctions/func1.js
@@ -5,7 +5,7 @@ module.exports = async (ctx) => {
     ctx.session.waitingForFile = true;
     ctx.session.currentFunctions = 'func1';
     
-    await ctx.reply('Вы выбрали: 1');
+    await ctx.reply('*Вы выбрали: 1*', {parse_mode: "Markdown"});
     await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
         Markup.keyboard([
             ['Вернуться в меню']
