a4f01bcf7934b7f597da935e8ff370d0681eced6
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2026-01-15 22:10:40 +0300

    Переход на ES-модуль

--- functions/recipientFunctions/func1Recipient.js
+++ functions/recipientFunctions/func1Recipient.js
@@ -1,6 +1,7 @@
-const { Markup } = require('telegraf');
+// отчет по расписанию
+import { Markup } from "telegraf";
 
-module.exports = async (ctx) => {
+export default async (ctx) => {
     ctx.session.isInFunctionMenu = false;
     ctx.session.waitingForFile = true;
     ctx.session.currentFunctions = 'func1';
