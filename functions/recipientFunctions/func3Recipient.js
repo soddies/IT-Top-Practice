8a5d17dc20b61de9857cdaf68a3ee6ad6a8454b7
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2026-01-15 22:11:38 +0300

    Переход на ES-модуль

--- functions/recipientFunctions/func3Recipient.js
+++ functions/recipientFunctions/func3Recipient.js
@@ -1,12 +1,12 @@
-// отчет по темам занятия
+// отчет по студентам
 import { Markup } from "telegraf";
 
 export default async (ctx) => {
     ctx.session.isInFunctionMenu = false;
     ctx.session.waitingForFile = true;
-    ctx.session.currentFunctions = 'func2';
+    ctx.session.currentFunctions = 'func3';
     
-    await ctx.reply('*Вы выбрали: 2*', {parse_mode: "Markdown"});
+    await ctx.reply('*Вы выбрали: 3*', {parse_mode: "Markdown"});
     await ctx.reply("Пожалуйста, загрузите файл в формате XLS/XLSX", 
         Markup.keyboard([
             ['Вернуться в меню']
