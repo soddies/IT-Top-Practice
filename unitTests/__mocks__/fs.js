df3ce6d81384f836d065dfdb6abf000788598092
Ekaterina Pchelkina <ktrn.pchelkina@gmail.com>
2026-01-21 15:46:08 +0300

    Delete unitTests/__mocks__ directory

--- unitTests/__mocks__/fs.js
+++
@@ -1,9 +0,0 @@
-export default {
-    writeFileSync: jest.fn(),
-    createReadaStream: jest.fn(() => ({
-        pipe: jest.fn(),
-        on: jest.fn()
-    })),
-    readFileSync: jest.fn(),
-    existsSync: jest.fn(() => true)
-};
--- unitTests/__mocks__/path.js
+++
@@ -1,7 +0,0 @@
-import * as actualPath from 'path'
-
-export default {
-    ...actualPath,
-    dirname: jest.fn(() => '/test/dir'),
-    join: jest.fn((...args) => args.join('/'))
-};
